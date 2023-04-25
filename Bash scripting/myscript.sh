5b801dc55480593e71095d2990b30dc2c958a96e
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-25 10:27:46 +0530

    initial commit

---
+++ Bash scripting/myscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+author="gowtham sankar"
+echo "Hello World!"
+
+sleep 1
+echo ""
+
+echo "My Current Working Directory is : "
+echo ""
+pwd
+
+sleep 1
+echo ""
+
+echo "A List Of The Files In The Directory : "
+echo ""
+ls -al
+
+sleep 1
+echo ""
+
+echo "This script author is $author"
+

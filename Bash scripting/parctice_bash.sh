f05bcd5129f49de18dc01bfe0af746217d79f085
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-25 10:29:30 +0530

    initial commit

---
+++ Bash scripting/parctice_bash.sh
@@ -0,0 +1,20 @@
+# @leaner gowtham sankar 
+
+#!/bin/bash
+
+echo "Today is "  `date`
+
+# Variables and data types in Bash
+echo "Enter your name "
+read user
+sleep 5
+
+echo "welcome " $user
+
+# echo -e "\nenter the path to directory"
+# read the_path
+
+# echo -e "\nyour path has the following files and folders: "
+# ls $the_path
+
+echo "I am using " $SHELL

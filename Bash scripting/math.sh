1848611b5c5605326478278a23e24773257849b2
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-29 16:13:45 +0530

    initial commit

---
+++ Bash scripting/math.sh
@@ -0,0 +1,13 @@
+expr 30 + 10
+
+expr 30 - 10
+
+# expr: syntax error: unexpected argument ‘math.sh’ ::  Don't use *
+expr 100 \* 4
+
+mynumber=100
+
+sleep 1
+echo $mynumber 
+
+expr $mynumber + 50
